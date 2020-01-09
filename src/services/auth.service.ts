3e0d673d4c110f2a693034d8c20954f6aa751911
Ricardo Simas <Rsimas@unigranrio.br>
2020-01-09 18:28:06 -0300

    Salvando dados do user logado no localStorage.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,11 +3,13 @@ import { CredenciaisDTO } from "../models/credenciais.dto";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
 import { text } from "@angular/core/src/render3/instructions";
+import { LocalUser } from "../models/local_user";
+import { StorageService } from "./storage.service";
 
 @Injectable()
 export class AuthService {
 
-    constructor(public http : HttpClient) {
+    constructor(public http : HttpClient, public storage: StorageService) {
 
     }
 
@@ -18,5 +20,19 @@ export class AuthService {
             observe : 'response',
             responseType : 'text'
         });
-    } 
+    }
+    
+    successfulLogin(authorizationValue : string){
+        let tok = authorizationValue.substring(7);
+
+        let user : LocalUser = {
+            token: tok
+        };
+
+        this.storage.setLocalUser(user);
+    }
+
+    logout(){
+        this.storage.setLocalUser(null);
+    }
 }
