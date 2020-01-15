6633a6b51a972d7aede3435d2f3ab697e83e7f5c
Ricardo Simas <Rsimas@unigranrio.br>
2020-01-15 17:24:41 -0300

    Salvando novo cliente.

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldMessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -36,6 +37,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                 case 403:
                     this.handle403();
                     break;
+                
+                case 422:
+                    this.handle422(errorObj);
+                    break;
 
                 default:
                     this.handleDefaultError(errorObj);
@@ -65,6 +70,21 @@ export class ErrorInterceptor implements HttpInterceptor {
         this.storage.setLocalUser(null);
     }
 
+    handle422(errorObj){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss : false,
+            buttons : [
+                {
+                    text: 'OK'
+                }
+            ]
+        })
+
+        alert.present();
+    }
+
     handleDefaultError(errorObj){
         
         let alert = this.alertCtrl.create({
@@ -81,6 +101,14 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
+    listErrors(messages: FieldMessage[]) : string{
+        let s : string = '';
+        for(var i=0; i<messages.length; i++){
+            s = s + '<p><strong>' + messages[i].fieldName + '</strong>: ' + messages[i].message + '</p>'
+        }
+
+        return s;
+    }
 }
 
 export const ErrorInterceptorProvider = {
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,24 +4,37 @@ import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
+import { ResponseType } from "@angular/http";
 
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http : HttpClient, public storage: StorageService) {
+    constructor(public http: HttpClient, public storage: StorageService) {
 
     }
 
-    findByEmail(email: String) : Observable<ClienteDTO> {
+    findByEmail(email: String): Observable<ClienteDTO> {
 
         return this.http.get<ClienteDTO>(
             `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
-    getImageFromBucket(id : String) : Observable<any> {
+    getImageFromBucket(id: String): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
 
-        return this.http.get(url, {responseType : 'blob'});
+        return this.http.get(url, { responseType: 'blob' });
+    }
+
+    insert(obj : ClienteDTO) {
+       
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes`,
+            obj,
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
     }
 }
