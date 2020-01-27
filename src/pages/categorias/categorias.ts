c400a91169296cc8a38ac51a174b2b24aeb63d72
Ricardo Simas <Rsimas@unigranrio.br>
2020-01-27 18:09:03 -0300

    Implementando componente loading.

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
 import { CategoriaDTO } from '../../models/categoria.dto';
 import { API_CONFIG } from '../../config/api.config';
@@ -18,8 +18,7 @@ export class CategoriasPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public categoriaService: CategoriaService
-    ) {
+    public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
